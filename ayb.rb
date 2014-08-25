7111572c894bf24656919bb124835ba97d5c2bf7
Zach Taylor <taylorzr@gmail.com>
2014-08-25 07:49:50 -0500

    Adds ayb

---
+++ ayb.rb
@@ -0,0 +1,17 @@
+require 'rye'
+
+dbc_hosts = (1..34).map{ |number| "dbc%02d.local" % number }
+user = 'apprentice'
+password = 'mvclover'
+
+rset = Rye::Set.new(parallel: true)
+
+dbc_hosts.each do |dbc_host|
+  box = Rye::Box.new("dbc20.local", user: user, safe: false, password: password, password_prompt: false)
+  rset.add_box(box)
+end
+
+rset.execute("osascript -e 'set volume 7'")
+rset.execute("say 'all your base are belong to us'")
+
+
